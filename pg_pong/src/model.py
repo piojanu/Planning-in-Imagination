c6ecda408193ba7c352bebcf30b1923e044eb481
Grzegorz Beringer <grzegorz.beringer@gmail.com>
2018-03-30 14:39:26 +0200

    Add Policy Gradient model

---
+++ pg_pong/src/model.py
@@ -0,0 +1,28 @@
+import torch.nn as nn
+
+
+class PolicyGradientModel(nn.Module):
+    """Neural network policy gradient model.
+    Contains 1 fully-connected hidden layer.
+    """
+    def __init__(self, input_size=6400, hidden_size=200, output_size=3,
+                 init_method='xavier'):
+        super(PolicyGradientModel, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, output_size),
+            nn.Softmax(dim=1)
+        )
+        self.initialize_weights(init_method)
+
+    def initialize_weights(self, init_method):
+        for layer in self.model.children():
+            if isinstance(layer, nn.Linear):
+                if init_method == 'xavier':
+                    nn.init.xavier_normal(layer.weight)
+                else:
+                    nn.init.normal(layer.weight)
+
+    def forward(self, x):
+        return self.model(x)
