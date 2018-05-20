61e2d8e681e8443db773fd1be40eae2583dfd238
piosobc <piosobc@gmail.com>
2018-05-20 21:30:35 +0200

    Upload env model. Move pong dits to src dir
    Add autoencoder, action-conditional env model

---
+++ src/env-model/models.py
@@ -0,0 +1,148 @@
+import torch.nn.functional as F
+import torch.nn as nn
+
+
+class GenerativeModel(nn.Module):
+    def __init__(self):
+        """
+        Model architecture based on "Action-Conditional Video Prediction using Deep Neural Networks" paper
+        url: https://arxiv.org/pdf/1507.08750.pdfs
+        """
+        nn.Module.__init__(self)
+
+        # Convolutional layers
+        self.conv1 = nn.Conv2d(1, 64, 8, stride=2, padding=3)
+        self.conv2 = nn.Conv2d(64, 128, 6, stride=2, padding=2)
+        self.conv3 = nn.Conv2d(128, 128, 6, stride=2, padding=2)
+        self.conv4 = nn.Conv2d(128, 128, 4, stride=2, padding=1)
+
+        # FC layer pre action-conditional transformation
+        self.fc1 = nn.Linear(128 * 5 * 5, 2048)
+        self.fc2 = nn.Linear(2048, 2048)
+
+        # Todo add action-conditional transformations
+
+        # FC layer pre action-conditional transformation
+        self.fc3 = nn.Linear(2048, 2048)
+        self.fc4 = nn.Linear(2048, 128 * 5 * 5)
+
+        # Deconv layers
+        self.deconv1 = nn.ConvTranspose2d(in_channels=128, out_channels=128, kernel_size=4, stride=2, padding=1)
+        self.deconv2 = nn.ConvTranspose2d(in_channels=128, out_channels=128, kernel_size=6, stride=2, padding=2)
+        self.deconv3 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=6, stride=2, padding=2)
+        self.deconv4 = nn.ConvTranspose2d(in_channels=64, out_channels=1, kernel_size=8, stride=2, padding=3)
+
+    def forward(self, state, action=None):
+        conv1_relu = F.relu(self.conv1(state))
+        conv2_relu = F.relu(self.conv2(conv1_relu))
+        conv3_relu = F.relu(self.conv3(conv2_relu))
+        conv4_relu = F.relu(self.conv4(conv3_relu))
+
+        flatten = conv4_relu.view(-1, 128 * 5 * 5)
+        fc1 = self.fc1(flatten)
+        fc2 = self.fc2(fc1)
+
+        # Todo implement action-conditional transformation
+
+        fc3 = self.fc3(fc2)
+        fc4 = self.fc4(fc3)
+        deflatten = fc4.view(-1, 128, 5, 5)
+
+        deconv1_relu = F.relu(self.deconv1(deflatten))
+        deconv2_relu = F.relu(self.deconv2(deconv1_relu))
+        deconv3_relu = F.relu(self.deconv3(deconv2_relu))
+        deconv4_relu = F.relu(self.deconv4(deconv3_relu))
+        return deconv4_relu
+
+
+class GenerativeModelMini(nn.Module):
+    def __init__(self, dense_size=500):
+        nn.Module.__init__(self)
+
+        # Convolutional layers
+        self.conv1 = nn.Conv2d(1, 16, 3, stride=3, padding=1)
+        self.conv2 = nn.Conv2d(16, 8, 3, stride=2, padding=1)
+
+        self.pool1 = nn.MaxPool2d(2, stride=2)
+        self.pool2 = nn.MaxPool2d(2, stride=1)
+
+        # dense layers
+        self.state_dense = nn.Linear(8 * 6 * 6, dense_size)
+        self.action_dense = nn.Linear(4, dense_size)
+        self.action_state_dense = nn.Linear(dense_size, 8 * 6 * 6)
+
+        # Deconv layer
+        self.deconv1 = nn.ConvTranspose2d(in_channels=8, out_channels=16, kernel_size=3, stride=2, padding=0)
+        self.deconv2 = nn.ConvTranspose2d(in_channels=16, out_channels=8, kernel_size=5, stride=3, padding=0)
+        self.deconv3 = nn.ConvTranspose2d(in_channels=8, out_channels=1, kernel_size=2, stride=2, padding=1)
+
+    def forward(self, state, action=None):
+        conv1_relu = F.relu(self.conv1(state))
+        conv1_pooled = self.pool1(conv1_relu)
+        conv2_relu = F.relu(self.conv2(conv1_pooled))
+        conv2_pooled = self.pool2(conv2_relu)
+
+        state_enc_flat = conv2_pooled.view(-1, 8 * 6 * 6)
+
+        # State dense
+        state_dense = self.state_dense(state_enc_flat)
+
+        # Action-conditional transformation
+        action_dense = self.action_dense(action)
+        state_action_tranformation = state_dense*action_dense
+
+        # Output dense
+        action_state_dense = self.action_state_dense(state_action_tranformation)
+
+        deflat = action_state_dense.view(-1, 8, 6, 6)
+
+        deconv1_relu = F.relu(self.deconv1(deflat))
+        deconv2_relu = F.relu(self.deconv2(deconv1_relu))
+        deconv3 = self.deconv3(deconv2_relu)
+        return F.tanh(deconv3)
+
+    @property
+    def name(self):
+        return "AE-action-conditional"
+
+
+class autoencoder(nn.Module):
+    def __init__(self, dense_size=500):
+        super(autoencoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 16, 3, stride=3, padding=1),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, stride=2),
+            nn.Conv2d(16, 8, 3, stride=2, padding=1),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, stride=1)  # b, 8, 2, 2
+        )
+
+        self.fc1 = nn.Linear(8 * 6 * 6, dense_size)
+        self.fc2 = nn.Linear(dense_size, 8 * 6 * 6)
+
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(8, 16, 3, stride=2),  # b, 16, 5, 5
+            nn.ReLU(True),
+            nn.ConvTranspose2d(16, 8, 5, stride=3, padding=0),  # b, 8, 15, 15
+            nn.ReLU(True),
+            nn.ConvTranspose2d(8, 1, 2, stride=2, padding=1),  # b, 1, 28, 28
+            nn.Tanh()
+        )
+
+    def forward(self, x, action=None):
+        # Encoding state
+        state_enc = self.encoder(x)
+
+        flatten = state_enc.view(-1, 8 * 6 * 6)
+        fc1 = self.fc1(flatten)
+        fc2 = self.fc2(fc1)
+
+        deflat = fc2.view(-1, 8, 6, 6)
+
+        output = self.decoder(deflat)
+        return output
+
+    @property
+    def name(self):
+        return "AE"
---
+++ src/env-model/train_model.py
@@ -0,0 +1,133 @@
+import torch, torch.optim as optim, torch.nn.functional as F
+from matplotlib import pyplot as plt
+import argparse
+from models import autoencoder, GenerativeModelMini
+from utils import eval_in_batches, test_model, load_data
+
+
+def run_eval(data, model):
+    restored_model = torch.load("{}.model".format(model.name))
+
+    val_loss = eval_in_batches(restored_model, data["val"]["states"], data["val"]["next_states"], data["val"]["actions"])
+    train_loss = eval_in_batches(restored_model, data["train"]["states"], data["train"]["next_states"], data["train"]["actions"])
+
+    print("Model evaluation: val_loss: {}, train_loss: {}".format(val_loss, train_loss))
+
+    test_model(restored_model, data["val"]["states"], data["val"]["next_states"], data["val"]["actions"])
+
+
+def run_training(data, model, batch_size=100, num_epochs=500, init_lr=0.0001, patience=10, eval_size=1000):
+    num_steps = data["train"]["states"].shape[0] // batch_size
+    intervals_without_improvement = 0
+
+    optimizer = optim.Adam(model.parameters(), lr=init_lr)
+
+    # Calculating reference loss as mean loss between state and next state
+    ref_loss_accumulated = 0
+    num_evals = data["train"]["states"].shape[0] // eval_size
+    for eval in range(num_evals):
+        loss = F.mse_loss(torch.Tensor(data["train"]["states"][eval * eval_size:(eval + 1) * eval_size]),
+                       torch.Tensor(data["train"]["next_states"][eval * eval_size:(eval + 1) * eval_size]))
+        ref_loss_accumulated += loss.item()
+    reference_loss = ref_loss_accumulated / num_evals
+
+    # Placeholders for losses history for plots
+    val_loss_hist = []
+    train_loss_hist = []
+    steps_hist = []
+
+    for epoch in range(num_epochs):
+        # Performing crossvaldiation
+        global_step = num_steps * epoch
+        # adjust_learning_rate(optimizer, init_lr, gamma=0.98, global_step=global_step, decay_interval=100)
+
+        val_loss = eval_in_batches(model, data["val"]["states"], data["val"]["next_states"], data["val"]["actions"])
+        train_loss = eval_in_batches(model, data["train"]["states"], data["train"]["next_states"], data["train"]["actions"])
+
+        print("Epoch: {}, step: {}, loss: [val: {}, train: {}, ref: {}], lr: {}".format(epoch, global_step,
+                                                                                        val_loss, train_loss,
+                                                                                        reference_loss,
+                                                                                        optimizer.param_groups[0][
+                                                                                            "lr"]))
+        steps_hist.append(global_step)
+        train_loss_hist.append(train_loss)
+        val_loss_hist.append(val_loss)
+
+        if val_loss == min(val_loss_hist):
+            print("val loss improved to {}. Saving model to {}".format(val_loss, "{}.model".format(model.name)))
+            torch.save(model, "{}.model".format(model.name))
+            intervals_without_improvement = 0
+        else:
+            intervals_without_improvement += 1
+
+        if intervals_without_improvement == patience:
+            break
+
+        for step in range(num_steps):
+            # Training
+            states_train_tensor = torch.Tensor(data["train"]["states"][step * batch_size:(step + 1) * batch_size])
+            next_states_train_tensor = torch.Tensor(
+                data["train"]["next_states"][step * batch_size:(step + 1) * batch_size])
+            actions_train_tensor = torch.Tensor(data["train"]["actions"][step * batch_size:(step + 1) * batch_size])
+
+            if states_train_tensor.shape != next_states_train_tensor.shape:
+                raise Exception("states and next states shape does not match!")
+
+            # predictions_train = model(states_train_tensor, actions_train_tensor)
+            predictions_train = model(states_train_tensor, actions_train_tensor)
+            loss = F.mse_loss(predictions_train, next_states_train_tensor)
+
+            model.zero_grad()
+
+            loss.backward()
+
+            optimizer.step()
+
+    print("Training finished, min val loss: {}, reference loss: {}, train loss: {}".format(min(val_loss_hist),
+                                                                                           reference_loss, train_loss))
+
+    plt.plot(steps_hist, val_loss_hist)
+    plt.plot(steps_hist, train_loss_hist)
+    plt.ylabel('MSE', fontsize=18)
+    plt.xlabel('global step', fontsize=18)
+    plt.legend(['validation error', 'train error'], fontsize=14)
+    plt.show()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--train", action="store_true")
+    parser.add_argument("--eval", action="store_true")
+    parser.add_argument("--dataset", default="buffer100k.npz")
+    parser.add_argument("--model", default="generative")
+    args = parser.parse_args()
+
+    # Loading data
+    data = load_data(args.dataset)
+
+    # Creating model
+    if args.model == "generative":
+        model = GenerativeModelMini()
+    elif args.model == "autoencoder":
+        model = autoencoder()
+    else:
+        raise Exception("Model has to be either generative or autoencoder")
+
+    # Choosing HW
+    if torch.cuda.is_available():
+        print("Running on GPU!")
+        torch.set_default_tensor_type('torch.cuda.FloatTensor')
+        model.cuda()
+    else:
+        print("Running on CPU!")
+        torch.set_default_tensor_type('torch.FloatTensor')
+
+    # Running training or evaluation
+    if args.train:
+        run_training(data, model)
+    elif args.eval:
+        run_eval(data, model)
+    else:
+        raise Exception("Please specify either train or eval param")
+
+
