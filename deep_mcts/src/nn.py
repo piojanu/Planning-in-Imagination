e06ed82560cd1e72a39e91051305a45ca12a45fc
Piotr Januszewski <piojanuszewski@gmail.com>
2018-06-08 23:51:00 +0200

    Declare NeuralNet interface and KerasNet abstract class

--- deep_mcts/src/nn.py
+++ deep_mcts/src/nn.py
@@ -1,15 +1,76 @@
 from abc import ABCMeta, abstractmethod
 
 
+class NeuralNet(metaclass=ABCMeta):
+    """Artificial neural mind of planning."""
+
+    @abstractmethod
+    def build(self, params={}):
+        """Build neural network model.
+
+        Args:
+            params (dict): Train/inference hyper-parameters.
+        """
+
+        pass
+
+    @abstractmethod
+    def predict(self, state):
+        """Do forward pass through nn, inference on state.
+
+        Args:
+            state (numpy.Array): State of game to inference on.
+
+        Returns:
+            numpy.Array: Inference result, depends on specific model.
+        """
+
+        pass
+
+    @abstractmethod
+    def train(self, data, targets):
+        """Perform training according to passed parameters in :method:`build` call.
+
+        Args:
+            data (numpy.Array): States to train on.
+            targets (numpy.Array): Ground truth targets, depend on specific model.
+        """
+
+        pass
+
+    @abstractmethod
+    def save_checkpoint(self, folder, filename):
+        """Saves the current neural network (with its parameters) in folder/filename.
+
+        Args:
+            folder (string): Directory for storing checkpoints.
+            filename (string): File name to save nn in, will have date/time appended.
+        """
+
+        pass
+
+    @abstractmethod
+    def load_checkpoint(self, folder, filename):
+        """Loads parameters of the neural network from folder/filename.
+
+        Args:
+            folder (string): Directory for loading checkpoints from.
+            filename (string): File name of saved nn checkpoint.
+        """
+
+        pass
+
+
 class KerasNet(metaclass=ABCMeta):
     """Artificial neural mind of planning."""
 
     @abstractmethod
-    def build(self, **kwargs):
+    def build(self, params={}):
         """Build neural network model in Keras.
 
         Args:
-            **kwargs: Hyper-parameters, depend on specific model implementation.
+            params (dict): Train/inference hyper-parameters. Available:
+              * '...' (...) : ...
         """
 
         pass
@@ -36,4 +97,26 @@ class KerasNet(metaclass=ABCMeta):
         """
 
         # TODO (pj): Implement training of Keras model on given data.
+        raise NotImplementedError()
+
+    def save_checkpoint(self, folder, filename):
+        """Saves the current neural network (with its parameters) in folder/filename.
+
+        Args:
+            folder (string): Directory for storing checkpoints.
+            filename (string): File name to save nn in, will have date/time appended.
+        """
+
+        # TODO (pj): Implement saving Keras model.
+        raise NotImplementedError()
+
+    def load_checkpoint(self, folder, filename):
+        """Loads parameters of the neural network from folder/filename.
+
+        Args:
+            folder (string): Directory for loading checkpoints from.
+            filename (string): File name of saved nn checkpoint.
+        """
+
+        # TODO (pj): Implement loading Keras model.
         raise NotImplementedError()
