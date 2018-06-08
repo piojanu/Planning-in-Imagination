ea9815eda5e5ee0627d7cd609d339d7b8c2b52e7
Mateusz Jablonski <matjabl1@student.pg.gda.pl>
2018-06-08 19:30:05 +0200

    Add docstrings to GameEnv

--- deep_mcts/src/env.py
+++ deep_mcts/src/env.py
@@ -3,7 +3,18 @@ from src.games import *  # This allows to create every game from board_games
 
 
 class GameEnv(Environment):
+    """Environment for board games from https://github.com/suragnair/alpha-zero-general
+    Available games:
+        - Connect4
+
+    """
+
     def __init__(self, name):
+        """Initialize Environment.
+
+        Args:
+            name (string): The name of game, in lower case. E.g. "connect4".
+        """
         self.game = eval(name)()
         self.reset()
         self._action_space_info = Environment.ActionSpaceInfo(
@@ -26,9 +37,21 @@ class GameEnv(Environment):
         return next_state, decoded_player, end, end != 0
 
     def reset(self, train_mode):
+        self.train_mode = train_mode
         self.player = 1
         self._curr_state = self.game.getInitBoard()
         return self._curr_state, self._decode_player(self.player)
 
     def _decode_player(self, player):
+        """Translate Game interface specific index to normalized value.
+        The rules are as follows:
+            1  -> 0
+            -1 -> 1
+
+        Args:
+            player (int): Encoded player index.
+
+        Returns:
+            (int): Decoded player index.
+        """
         return 0 if player == 1 else -1
