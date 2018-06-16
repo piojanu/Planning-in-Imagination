783ea7d7adbead9f46a9e1e969af16c7321721ff
Grzegorz Beringer <grzegorz.beringer@gmail.com>
2018-06-16 23:46:34 +0200

    Add reward prediction to env model
    Other changes:
    - Fix prefetch dataset window creation
    - Add fake 0 rewards to NpzData, for compatibility
    - Rebuild training loop to make things faster and
      more verbose

--- trivial_env_model/train_model.py
+++ trivial_env_model/train_model.py
@@ -1,6 +1,7 @@
 import torch, torch.optim as optim, torch.nn.functional as F
 import argparse
 import time
+import numpy as np
 from models import GenerativeModelMini
 from utils import eval_in_batches, test_model
 from dataset import get_data
@@ -15,12 +16,14 @@ def run_eval(data, model):
     val_loss = eval_in_batches(restored_model, data.valid_set)
     train_loss = eval_in_batches(restored_model, data.train_set)
 
-    print("Model evaluation: test_loss: {}, val_loss: {}, train_loss: {}".format(test_loss, val_loss, train_loss))
+    print("Model evaluation: test_loss: {:.5f}, val_loss: {:.5f}, train_loss: {:.5f}"
+          .format(test_loss, val_loss, train_loss))
 
     test_model(restored_model, data.test_set)
 
 
-def run_training(data, model, batch_size=100, num_epochs=1000, init_lr=0.0015, patience=30, eval_size=1000):
+def run_training(data, model, batch_size=100, num_epochs=1000, init_lr=0.0015, patience=30, eval_size=1000,
+                 reward_loss_factor=0.001, log_step=100):
     num_steps = data.train_set.num_samples // batch_size
     intervals_without_improvement = 0
 
@@ -45,23 +48,53 @@ def run_training(data, model, batch_size=100, num_epochs=1000, init_lr=0.0015, p
 
     for epoch in range(num_epochs):
         # Performing crossvaldiation
-        global_step = num_steps * epoch
         # adjust_learning_rate(optimizer, init_lr, gamma=0.98, global_step=global_step, decay_interval=100)
+
+        train_time = time.time()
+        state_losses = []
+        reward_losses = []
+        losses = []
+        for step in range(num_steps):
+            # Training
+            states_batch, next_states_batch, actions_batch, rewards_batch = data.train_set.get_next_batch(batch_size)
+            states_train_tensor = torch.Tensor(states_batch)
+            next_states_train_tensor = torch.Tensor(next_states_batch)
+            actions_train_tensor = torch.Tensor(actions_batch)
+            rewards_train_tensor = torch.Tensor(rewards_batch)
+
+            assert states_train_tensor.shape[0] == next_states_train_tensor.shape[0], \
+                "states and next states shape does not match!"
+
+            next_states_pred, rewards_pred = model(states_train_tensor, actions_train_tensor)
+            state_loss = F.mse_loss(next_states_pred, next_states_train_tensor)
+            reward_loss = reward_loss_factor * F.mse_loss(rewards_pred, rewards_train_tensor)
+            loss = state_loss + reward_loss
+
+            state_losses.append(state_loss.item())
+            reward_losses.append(reward_loss.item())
+            losses.append(loss.item())
+
+            model.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if step % log_step == 0:
+                print("Step {}/{}: loss: {:.5f}, state_loss: {:.5f}, reward_loss: {:.5f}"
+                      .format(step+1, num_steps, np.mean(losses), np.mean(state_losses), np.mean(reward_losses)))
+
         eval_time = time.time()
-        val_loss = eval_in_batches(model, data.valid_set)
-        train_loss = eval_in_batches(model, data.train_set)
+        train_loss = np.mean(losses)
+        val_loss = eval_in_batches(model, data.valid_set, reward_loss_factor)
         eval_time = time.time() - eval_time
-        print("Epoch: {}, step: {}, loss: [val: {}, train: {}, ref: {}], lr: {}".format(epoch, global_step,
-                                                                                        val_loss, train_loss,
-                                                                                        reference_loss,
-                                                                                        optimizer.param_groups[0][
-                                                                                            "lr"]))
+        global_step = num_steps * (epoch + 1)
+        print("Epoch: {}, step: {}, loss: [train: {:.5f}, val: {:.5f}, ref: {:.5f}], lr: {}"
+              .format(epoch+1, global_step, train_loss, val_loss, reference_loss, optimizer.param_groups[0]["lr"]))
         steps_hist.append(global_step)
         train_loss_hist.append(train_loss)
         val_loss_hist.append(val_loss)
 
         if val_loss == min(val_loss_hist):
-            print("val loss improved to {}. Saving model to {}".format(val_loss, "{}.model".format(model.name)))
+            print("val loss improved to {:.5f}. Saving model to {}".format(val_loss, "{}.model".format(model.name)))
             torch.save(model, "{}.model".format(model.name))
             intervals_without_improvement = 0
         else:
@@ -69,25 +102,6 @@ def run_training(data, model, batch_size=100, num_epochs=1000, init_lr=0.0015, p
 
         if intervals_without_improvement == patience:
             break
-        train_time = time.time()
-        for step in range(num_steps):
-            # Training
-            states_batch, next_states_batch, actions_batch = data.train_set.get_next_batch(batch_size)
-            states_train_tensor = torch.Tensor(states_batch)
-            next_states_train_tensor = torch.Tensor(next_states_batch)
-            actions_train_tensor = torch.Tensor(actions_batch)
-
-            assert states_train_tensor.shape[0] == next_states_train_tensor.shape[0], "states and next states shape does not match!"
-
-            # predictions_train = model(states_train_tensor, actions_train_tensor)
-            predictions_train = model(states_train_tensor, actions_train_tensor)
-            loss = F.mse_loss(predictions_train, next_states_train_tensor)
-
-            model.zero_grad()
-
-            loss.backward()
-
-            optimizer.step()
         scheduler.step()
     train_time = time.time() - train_time
     print("Eval time: {}. train time {}".format(eval_time, train_time))
--- trivial_env_model/utils.py
+++ trivial_env_model/utils.py
@@ -22,18 +22,19 @@ def adjust_learning_rate(optimizer, init_lr, gamma, global_step, decay_interval)
         param_group['lr'] = lr
 
 
-def eval_in_batches(model, dataset, eval_size=1000):
+def eval_in_batches(model, dataset, reward_loss_factor=0.01, eval_size=1000):
     if eval_size > dataset.num_samples:
         eval_size = dataset.num_samples
     num_batches = dataset.num_samples//eval_size
     loss_accumulated = 0
 
     for i in range(num_batches):
-        states_batch, next_states_batch, actions_batch = dataset.get_next_batch(eval_size)
-        predictions = model(torch.Tensor(states_batch), torch.Tensor(actions_batch))
-        loss = F.mse_loss(predictions, torch.Tensor(next_states_batch))
+        states_batch, next_states_batch, actions_batch, rewards_batch = dataset.get_next_batch(eval_size)
+        states_pred, rewards_pred = model(torch.Tensor(states_batch), torch.Tensor(actions_batch))
+        loss = F.mse_loss(states_pred, torch.Tensor(next_states_batch))
+        rewards_loss = reward_loss_factor * F.mse_loss(rewards_pred, torch.Tensor(rewards_batch))
 
-        loss_accumulated += loss.item()
+        loss_accumulated += loss.item() + rewards_loss.item()
 
     return loss_accumulated/num_batches
 
@@ -42,14 +43,14 @@ def test_model(model, dataset, interval=0.003):
     fig = plt.figure(figsize=(1, 3))
     model = torch.load("{}.model".format(model.name))
     for i in range(dataset.num_samples):
-        states, next_states, actions = dataset.get_next_batch(1)
-        prediction = model(torch.unsqueeze(torch.Tensor(states[0]), 0), torch.unsqueeze(torch.Tensor(actions[0]), 0))
+        states, next_states, actions, rewards = dataset.get_next_batch(1)
+        state_pred, reward_pred = model(torch.Tensor(states), torch.Tensor(actions))
 
         fig.add_subplot(1, 3, 1)
+        plt.suptitle("Step: {}, reward_pred: {:.5f}".format(i, reward_pred.detach().cpu().numpy()[0][0]))
         plt.imshow(states[0][-1].astype(np.float32))
         fig.add_subplot(1, 3, 2)
-        plt.imshow(prediction.detach().cpu().numpy().reshape(80, 80))
-        plt.title("Step: {}".format(i))
+        plt.imshow(state_pred.detach().cpu().numpy().reshape(80, 80))
         fig.add_subplot(1, 3, 3)
         plt.imshow(next_states[0][0].astype(np.float32))
 
