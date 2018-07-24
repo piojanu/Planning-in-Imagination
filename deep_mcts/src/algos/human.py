4e98b5634a8caf6c7f4fc35a67d9f52f18c7ebc3
Grzegorz Beringer <grzegorz.beringer@gmail.com>
2018-07-24 21:03:58 +0200

    Make action values constant for human play
    Action is now connected directly to a place on the board
    and doesn't change depending on available actions.

--- deep_mcts/src/algos/human.py
+++ deep_mcts/src/algos/human.py
@@ -22,28 +22,26 @@ class HumanPlayer(hrl.Mind):
             logits = np.zeros(self.game.get_action_size(), dtype=np.float32)
             logits[-1] = 1.0
         else:
-            action_range = range(1, len(valid_actions) + 1)
-
             # Create a board with available actions represented as int values from 1 to n_actions.
             # Invalid actions are representede with NaN, which is rendered as "-".
             available_actions_board = np.full(state.shape, np.nan)
             np.set_printoptions(nanstr="-")
-            np.put(available_actions_board, valid_actions, action_range)
+            np.put(available_actions_board, valid_actions, valid_actions)
 
             print("\nAvailable actions:")
             print("------------------")
             print(available_actions_board)
 
             action = -1
-            while action not in action_range:
+            while action not in valid_actions:
                 action_str = input("Action: ")
                 try:
                     action = int(action_str)
-                    if action not in action_range:
-                        print("Given action is not available! Please input a valid action!"
-                              .format(action_range))
+                    if action not in valid_actions:
+                        print("Given action is not available! Please input a valid action! Available: {}"
+                              .format(valid_actions))
                 except ValueError:
-                    print("Please input a valid integer!".format(action_range))
+                    print("Please input a valid integer!")
 
             # Create "logits" with all elements equal to 0, and taken action equal to 1.
             logits = np.zeros(self.game.get_action_size(), dtype=np.float32)
