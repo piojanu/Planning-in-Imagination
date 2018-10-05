74f1a74992a15759ce1f26acffb6a2132094542f
Mateusz Jablonski <matjabl1@student.pg.gda.pl>
2018-10-05 21:39:52 +0200

    Alphazero: add test for train command and refactor testing script

--- alphazero/scripts/run_cmds.sh
+++ alphazero/scripts/run_cmds.sh
@@ -2,11 +2,20 @@
 CKPT_DIR=checkpoints/test/
 MAIN_SCRIPT_PATH=../src/run.py
 
-echo "Testing self_play"
-python $MAIN_SCRIPT_PATH -c test_config.json self_play || { echo "self_play Failed" ; exit 1; }
+run_test () {
+  echo "Testing $1"
+  python $MAIN_SCRIPT_PATH -c test_config.json ${@:1:99} >test.log 2>error.log && echo "$1 succeeded" || { cat test.log && cat error.log && echo "$1 failed" && exit 1; }
+}
 
-echo "Testing cross_play"
-python $MAIN_SCRIPT_PATH -c test_config.json cross_play -d $CKPT_DIR || { echo "cross_play Failed" ; exit 1; }
+run_test self_play
 
-echo "Testing clash"
-python $MAIN_SCRIPT_PATH -c test_config.json clash --no-render $CKPT_DIR`ls $CKPT_DIR | cut -f 1 | head -n1` $CKPT_DIR`ls $CKPT_DIR | cut -f 1 | tail -n1` || { echo "clash Failed" ; exit 1; }
+run_test cross_play -d $CKPT_DIR
+
+run_test clash --no-render $CKPT_DIR`ls $CKPT_DIR | cut -f 1 | head -n1` $CKPT_DIR`ls $CKPT_DIR | cut -f 1 | tail -n1`
+
+run_test train -ckpt $CKPT_DIR`ls $CKPT_DIR | cut -f 1 | tail -n1`
+
+#TODO: enable it
+#run_test hopt -n 20
+
+rm -rf checkpoints
